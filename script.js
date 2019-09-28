2c9c5ff80a1f16f3a5c8779b8fb98ad2062b6167
chris.dickinson <chris.dickinson@autotrader.co.uk>
2019-09-28 10:33:35 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,63 @@
+tabluar = () => {
+  const element = document.getElementById("tabular");
+  if (element.classList.contains("tabular")) {
+    element.classList.remove("tabular");
+  } else {
+    element.classList.add("tabular");
+  }
+};
+
+
+old = () => {
+  const element = document.getElementById("old");
+  if (element.classList.contains("old")) {
+    element.classList.remove("old");
+  } else {
+    element.classList.add("old");
+  }
+};
+
+oldTab = () => {
+  const element = document.getElementById("oldTab");
+  if (element.classList.contains("oldTab")) {
+    element.classList.remove("oldTab");
+  } else {
+    element.classList.add("oldTab");
+  }
+};
+
+smcp = () => {
+  const element = document.getElementById("smcp");
+  if (element.classList.contains("smcp")) {
+    element.classList.remove("smcp");
+  } else {
+    element.classList.add("smcp");
+  }
+};
+
+smcpTab = () => {
+  const element = document.getElementById("smcpTab");
+  if (element.classList.contains("smcpTab")) {
+    element.classList.remove("smcpTab");
+  } else {
+    element.classList.add("smcpTab");
+  }
+};
+
+salt = () => {
+  const element = document.getElementById("salt");
+  if (element.classList.contains("salt")) {
+    element.classList.remove("salt");
+  } else {
+    element.classList.add("salt");
+  }
+};
+
+liga = () => {
+  const element = document.getElementById("liga");
+  if (element.classList.contains("liga")) {
+    element.classList.remove("liga");
+  } else {
+    element.classList.add("liga");
+  }
+};
