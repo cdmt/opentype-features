3ff087363812e36f825b28a1428855d9a55b8561
chris.dickinson <chris.dickinson@autotrader.co.uk>
2019-09-30 22:00:49 +0100

    style updates

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-opentypeFeature = (feature) => {
+opentypeFeature = (feature, title1, title2) => {
   const element = document.getElementById(feature);
+  let btn = event.target.innerText
   if (element.classList.contains(feature)) {
     element.classList.remove(feature);
   } else {
     element.classList.add(feature);
   }
+  if (event.target.innerText == title1) {
+    event.target.innerText = title2
+  } else {
+    event.target.innerText = title1
+  }
+
 }
